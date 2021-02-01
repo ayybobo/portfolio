3227f4e2e8ae50f9a0704cbdb5b5f7d1bcf45be4
ayybobo <jbowman.exe@gmail.com>
2021-01-31 18:47:55 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-// Projects grid
+// Projects grid - image darken on hover
 
 const portfolioItems = document.querySelectorAll(".portfolio-item-wrapper");
 
@@ -12,4 +12,34 @@ portfolioItems.forEach((portfolioItem) => {
     portfolioItem.childNodes[1].classList.remove("img-darken");
   });
 });
+
+// Hero Text animation
+const text = document.querySelector(".hero-text");
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+
+
+for (let i=0; i < splitText.length; i++) {
+  text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 150);
+
+function onTick() {
+  const span = text.querySelectorAll('span')[char];
+  span.classList.add('fade');
+  char++
+  if (char === splitText.length) {
+    complete();
+    return;
+  }
+}
+
+function complete() {
+  clearInterval(timer);
+  timer = null;
+}
+
 
